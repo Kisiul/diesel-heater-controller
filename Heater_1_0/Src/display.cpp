c1381fcf9107f76fef9df244ce34d69ada4776d6
Kisiul <krzysztof.dab@interia.eu>
2017-10-12 00:25:21 +0200

    some testing

--- Heater_1_0/Src/display.cpp
+++ Heater_1_0/Src/display.cpp
@@ -11,8 +11,8 @@ void display::pixel(UG_S16 x , UG_S16 y ,UG_COLOR c){
 }
 display::display(){
 	void (display::*func) (UG_S16 x , UG_S16 y ,UG_COLOR c);
-	func = &display::pixel;
-	for(int i =0; i<3; i++);
+	//func = &display::pixel;
+	//for(int i =0; i<3; i++);
 	//UG_Init(gui, func, 128, 128);
 }
 display::~display(){
